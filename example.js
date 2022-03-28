34ed5baa18854f85461907a91d1575c55b495be3
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-28 14:35:50 -0400

    es6 function sandbox

--- example.js
+++ example.js
@@ -1,36 +1,23 @@
-function myFunction(arg1, arg2) {
-  // function body
-}
+const sayHello = (name) => console.log(`hello ${name}`);
 
-const myOtherFunction = function (arg1, arg2) {
-  // function body
+const sayHello2 = sayHello;
+sayHello2('Matty');
+
+// Ternary operations
+const myFunc = true
+  ? () => console.log('First option..')
+  : () => console.log('Second option..');
+
+const DEVELOPMENT = true;
+
+const fetchDataReal = () => {
+  // time-intensive operations here!
 };
 
-// Arrow Functions
-const add = (x, y) => x + y;
-
-// ES6 shorthand
-const double = (number) => number * 2;
-
-// ES5
-const otherDouble = function (number) {
-  return number * 2;
-};
-
-const sayHello = () => console.log('hello');
-
-// implicit return
-const getPersonData = () => ({
-  name: 'John Doe',
-  age: 34,
-  job: 'programmer',
+const fetchDataFake = () => ({
+  name: 'John',
+  age: 43,
 });
 
-// 0 args
-const arrow1 = () => {};
-
-// 1 arg
-const arrow2 = (arg1) => {};
-
-// 2 arg
-const arrow3 = (arg1, arg2) => ({ name: 'hi', why: 'unsure' });
+// used at runtime? ternary condition
+const fetchData = DEVELOPMENT ? fetchDataFake : fetchDataReal;
