f358753c1024e5610eaa7183260d0dd1a2fdede3
Matthew <ricklefsmatthew1003@gmail.com>
2022-04-18 16:15:22 -0400

    begins linked list study

---
+++ linkedLists/example.js
@@ -0,0 +1,16 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+
+a.next = b;
+b.next = c;
+c.next = d;
