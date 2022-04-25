9c3d086c4562e94614244907bbdc658ae008267f
Matthew <ricklefsmatthew1003@gmail.com>
2022-04-25 17:44:28 -0400

    commits iterative and recursive solutions for merging two linked lists

--- linkedLists/zipperLists.js
+++ linkedLists/zipperLists.js
@@ -43,3 +43,18 @@ const zipperLists = (head1, head2) => {
 };
 
 zipperLists(a, b);
+
+// Recursive Solution
+const zipperLists = (head1, head2) => {
+  if (head1 === null && head2 === null) return null;
+  if (head1 === null) return head2;
+  if (head2 === null) return head1;
+
+  const next1 = head1.next;
+  const next2 = head2.next;
+  head1.next = head2;
+  head2.next = zipperLists(next1, next2);
+  return head1;
+};
+
+zipperLists(a, b);
