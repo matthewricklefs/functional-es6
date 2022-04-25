1fe26910b873e282f1bf05984dd59c95cfaca9e8
Matthew <ricklefsmatthew1003@gmail.com>
2022-04-25 17:15:14 -0400

    recursive and iterative approach at zipper lists coding challenge

--- linkedLists/zipperLists.js
+++ linkedLists/zipperLists.js
@@ -16,4 +16,30 @@ b.next = c;
 c.next = d;
 d.next = e;
 
-const zipperLists = (head1, head2) => {};
+// Iterative Solution
+const zipperLists = (head1, head2) => {
+  let tail = head1;
+  let current1 = head1;
+  let current2 = head2;
+  let count = 0;
+
+  while (current1 !== null && current2 !== null) {
+    if (count % 2 === 0) {
+      tail.next = current2;
+      current2 = current2.next;
+    } else {
+      tail.next = current1;
+      current1 = current1.next;
+    }
+
+    tail = tail.next;
+    count += 1;
+  }
+
+  if (current1 !== null) tail.next = current1;
+  if (current2 !== null) tail.next = current2;
+
+  return head1;
+};
+
+zipperLists(a, b);
